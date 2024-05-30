e4af4386935511c5d3d1281981140af2946a7835
jlira <jobson.lira@gmail.com>
2024-05-30 18:44:12 -0300

    arquivo signals

--- apps/filme/apps.py
+++ apps/filme/apps.py
@@ -6,12 +6,4 @@ class FilmeConfig(AppConfig):
     name = 'apps.filme'
 
     def ready(self):
-        from .models import Usuario
-        import os
-
-        email = os.getenv("EMAIL_ADMIN")
-        senha = os.getenv("SENHA_ADMIN")
-
-        usuarios = Usuario.objects.filter(email=email)
-        if not usuarios:
-            Usuario.objects.crete_superuser(username="admin", email=email, password=senha, is_active=True, is_staff=True)
+        import apps.filme.signals
